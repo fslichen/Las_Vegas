d587427c162f0a9716de8fcfbbdee1e9c164eae3
fslic <fslic@DESKTOP-MCATCHJ>
2017-05-21 23:26:36 +0800

    refactor

--- Las_Vegas/src/main/java/evolution/Application.java
+++ Las_Vegas/src/main/java/evolution/Application.java
@@ -3,10 +3,10 @@ package evolution;
 public class Application {
 	public static void main(String[] args) throws Exception {
 		LasVegas lasVegas = new LasVegas();
-		String str = lasVegas.encrypt("/Users/chenli/Desktop/SwaggerFactory.java",
-				"/Users/chenli/Desktop/Encrypted_SwaggerFactory.java");
+		String str = lasVegas.encrypt("D:/Buffer/Ext_Framework.zip",
+				"D:/Buffer/Ext_Framework_Encoded.txt");
 		System.out.println(str);
-		lasVegas.decrypt("/Users/chenli/Desktop/Encrypted_SwaggerFactory.java", 
-				"/Users/chenli/Desktop/Decrypted_SwaggerFactory.java");
+		lasVegas.decrypt("D:/Buffer/Ext_Framework_Encoded.txt", 
+				"D:/Buffer/Ext_Framework_Decoded.zip");
 	}
 }
